ba89d12aa1c452371a22a4508a8fc6811ce4245e
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-08-30 16:51:20 +0430

    Add the minimal server and the router register function

---
+++ routes/registerRouters.js
@@ -0,0 +1,4 @@
+// This function will register the routers
+async function registerRouters(fastify) {}
+
+module.exports = registerRouters;
---
+++ server.js
@@ -0,0 +1,32 @@
+const fastify = require("fastify")({ logger: true });
+const PORT = 3200;
+const registerRouters = require("./routes/registerRouters");
+
+// Register the fastify swagger
+fastify.register(require("fastify-swagger"), {
+  routePrefix: "/docs",
+  swagger: {
+    info: {
+      title: "Search API",
+      description: "Search API",
+      version: "1.0.0",
+    },
+  },
+  exposeRoute: true,
+});
+
+// Register  the routers
+registerRouters(fastify);
+
+// Start server function
+async function startServer() {
+  try {
+    await fastify.listen(PORT);
+  } catch (error) {
+    // if an error occurs print the error and exit the program
+    fastify.log.error(error);
+    process.exit(1);
+  }
+}
+
+startServer();
