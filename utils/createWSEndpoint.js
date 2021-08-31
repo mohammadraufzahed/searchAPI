33927db188b8810a313477994665350093cf341a
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-08-31 18:54:27 +0430

    Add the wsEndpoint creator

---
+++ utils/createWSEndpoint.js
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer");
+const path = require("path");
+const fs = require("fs");
+
+async function createWSEndpoint() {
+  const cachPath = path.normalize(__dirname + "/../.cach");
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: cachPath,
+    args: [
+      "--disable-canvas-aa", // Disable antialiasing on 2d canvas
+      "--disable-2d-canvas-clip-aa", // Disable antialiasing on 2d canvas clips
+      "--disable-gl-drawing-for-tests", // BEST OPTION EVER! Disables GL drawing operations which produce pixel output. With this the GL output will not be correct but tests will run faster.
+      "--disable-dev-shm-usage", // ???
+      "--no-zygote", // wtf does that mean ?
+      "--use-gl=desktop", // better cpu usage with --use-gl=desktop rather than --use-gl=swiftshader, still needs more testing.
+      "--enable-webgl",
+      "--hide-scrollbars",
+      "--mute-audio",
+      "--no-first-run",
+      "--disable-infobars",
+      "--disable-breakpad",
+      //'--ignore-gpu-blacklist',
+      "--no-sandbox", // meh but better resource comsuption
+      "--disable-setuid-sandbox", // same
+    ],
+  });
+  const browserEndpoint = await browser.wsEndpoint();
+  fs.writeFileSync(
+    path.normalize(__dirname + "/../wsEndpoint.json"),
+    JSON.stringify({ wsEndpoint: browserEndpoint }),
+    {
+      encoding: "utf-8",
+      flag: "w+",
+    }
+  );
+  await browser.disconnect();
+}
+
+createWSEndpoint();
